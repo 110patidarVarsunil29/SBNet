c3e80e5eadb409c65b8148b37e44f7ecb76a8907
Sunil Patidar <patidarsunil110@gmail.com>
2021-03-22 19:54:52 +0530

    Initial Commit by patidarsunil110@gmail.com

---
+++ src/main/java/com/rakutenIndia/Netocity/NetocityApplication.java
@@ -0,0 +1,13 @@
+package com.rakutenIndia.Netocity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetocityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetocityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rakutenIndia/Netocity/NetocityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rakutenIndia.Netocity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetocityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
