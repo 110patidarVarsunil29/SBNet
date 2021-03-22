da1422b0654b6e79d0465856ada4c48d996517e7
win10 <win10@DESKTOP-V5G3T1L>
2021-03-22 20:03:53 +0530

    commmit

--- src/main/java/com/rakutenIndia/Netocity/NetocityApplication.java
+++ src/main/java/com/rakutenIndia/Netocity/NetocityApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class NetocityApplication {
 
+	//Spring boot main class
 	public static void main(String[] args) {
 		SpringApplication.run(NetocityApplication.class, args);
 	}
